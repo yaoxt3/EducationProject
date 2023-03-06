4c6137e637432e52df98664910a55be8436aad23
yaoxt3 <759741745@qq.com>
2023-03-06 16:59:23 +0100

    fixed bugs

--- curi_sim/impedance_control_all_ik.py
+++ curi_sim/impedance_control_all_ik.py
@@ -18,10 +18,9 @@ from dm_control import mujoco
 from dm_control.mujoco.testing import assets
 from dm_control.mujoco.wrapper import mjbindings
 from dm_control.utils import inverse_kinematics as ik
-from dm_control.mujoco.testing import assets
 mjlib = mjbindings.mjlib
 
-_ARM_XML = assets.get_contents('/home/yxt/Research/Teng/EducationProject/curi_sim/description/Franka/arm_for_IK.xml')
+_ARM_XML = assets.get_contents('/home/yxt/Research/code/EducationProject/curi_sim/description/Franka/arm_for_IK.xml')
 _SITE_NAME = 'ee_joint'
 _JOINTS = ['panda0_joint1', 'panda0_joint2', 'panda0_joint3', 'panda0_joint4', 'panda0_joint5', 'panda0_joint6', 'panda0_joint7']
 _TOL = 1.2e-8
@@ -29,21 +28,21 @@ _MAX_STEPS = 150
 _MAX_RESETS = 10
 # -----------------------------------------
 
+# joint limitation
 joint_max = [2.8973, 1.7628, 2.8973, -0.4, 2.8973, 2.1127, 2.8973]
 joint_min = [-2.8973, -1.7628, -2.8973, -3.0718, -2.8973, -1.6573, -2.8973]
-
-
+    
+# set starting joint position
 def ik_set_joint_pos(physics, joint_pos):
     physics.named.data.qpos[_JOINTS] = joint_pos
+
 
 def inverse_kinematics(target_pos, target_quat, current_pos):
     physics = mujoco.Physics.from_xml_string(_ARM_XML)
     count = 0
     physics2 = physics.copy(share_model=True)
-    # resetter = _ResetArm(seed=0)
     ik_set_joint_pos(physics2, current_pos)
     step = 0
-    curr_time = time.time()
     while True:
         result = ik.qpos_from_site_pose(
             physics=physics2,
@@ -65,26 +64,45 @@ def inverse_kinematics(target_pos, target_quat, current_pos):
             raise RuntimeError(
                 'Failed to find a solution within %i attempts.' % _MAX_RESETS)
         step += 1
-        
-    print(f"ik time: {time.time() - curr_time}") 
-    
-    # physics.data.qpos[:] = result.qpos
-    # mjlib.mj_fwdPosition(physics.model.ptr, physics.data.ptr)
-    # if target_pos is not None:
-    #   pos = physics.named.data.site_xpos[_SITE_NAME]
-    #   np.testing.assert_array_almost_equal(pos, target_pos)
-    
-    # if target_quat is not None:
-    #   xmat = physics.named.data.site_xmat[_SITE_NAME]
-    #   quat = np.empty_like(target_quat)
-    #   mjlib.mju_mat2Quat(quat, xmat)
-    #   quat /= quat.ptp()  # Normalize xquat so that its max-min range is 1
-    #   np.testing.assert_array_almost_equal(quat, target_quat)
-      
+              
     return result.qpos
+
+
+# inverse kinematics thread
+def inverse_kinematics_thread():
+    global physics2, ik_target_pos, ik_target_quat, ik_current_pos, ik_joint_pos
+    count = 0
+    physics2.named.data.qpos[_JOINTS] = ik_current_pos
+    step = 0
+    while True:
+        step = 0
+        while True:
+            result = ik.qpos_from_site_pose(
+                physics=physics2,
+                site_name=_SITE_NAME,
+                target_pos=ik_target_pos,
+                target_quat=ik_target_quat,
+                joint_names=_JOINTS,
+                tol=_TOL,
+                max_steps=_MAX_STEPS,
+                inplace=False,
+            )
+            if result.success:
+                break
+            elif count < _MAX_RESETS:
+                physics2.named.data.qpos[_JOINTS] = ik_current_pos
+                count += 1
+            else:
+                raise RuntimeError(
+                    'Failed to find a solution within %i attempts.' % _MAX_RESETS)
+            step += 1
+        
+        ik_joint_pos = result.qpos
+        time.sleep(0.01)
+
       
       
-# --------- Modify as required ------------
+# --------- Modify as required ------------ #
 # Task-space controller parameters
 # stiffness gains
 P_pos = 1800.
@@ -92,25 +110,23 @@ P_ori = 450.
 # damping gains
 D_pos = 2.*np.sqrt(P_pos)
 D_ori = 2.
-# -----------------------------------------
+# ---------------------------------------- #
 
 def compute_ts_force(curr_pos, curr_ori, goal_pos, goal_ori, curr_vel, curr_omg, goal_vel):
     delta_pos = (goal_pos - curr_pos).reshape([3, 1])
     delta_ori = quatdiff_in_euler(curr_ori, goal_ori).reshape([3, 1])
-    # print(f"delta_ori:{delta_ori}")
-    # delta_vel = (goal_vel - curr_vel).reshape([3, 1])
     F = np.vstack([P_pos*(delta_pos), P_ori*(delta_ori)]) - \
         np.vstack([D_pos*(curr_vel).reshape([3, 1]),
                    D_ori*(curr_omg).reshape([3, 1])]) 
 
-    # print(f"force:{F}, shape:{np.shape(F)}")
     error = np.linalg.norm(delta_pos) + np.linalg.norm(delta_ori) 
 
     return F, error
 
-#
+
 def impedance_control_integration(ctrl_rate):
-    global env, target_pos, target_vel, original_ori, joint_controller, robot
+    global env, target_pos, target_vel, original_ori, joint_controller, robot, run_controller
+    global ik_target_pos, ik_target_quat, ik_current_pos, ik_joint_pos
     count = 0
     threshold = 0.0000005
 
@@ -164,7 +180,7 @@ def impedance_control_integration(ctrl_rate):
                     force_norm = np.sqrt(np.sum(np.square(c_array[0:3])))
                 else:
                     force_norm = 0
-                
+                 
             pub_f.publish(force_norm)
             total_force += force_norm
             
@@ -174,44 +190,26 @@ def impedance_control_integration(ctrl_rate):
             while force_norm > 0:
                 if count == 0:
                     target_joint = env.joint_position()[:7]
-
-                    # print(f"current ee pos:{env.get_ee_pose()}")
-                    # print(f"target_ee_pos:{target_ee_pos}, target_ee_ori:{target_ee_ori}")
-                    # ik_joint_pos = inverse_kinematics(target_ee_pos, target_ee_ori, target_joint)
-                    # print(f"ik joint pos:{ik_joint_pos}")
-                    # print(f"joint pos:{env.joint_position()[:7]}")
-                    # print("++++++++++++++++++++++++")
                     switch_controller = 2
                 count += 1
                 break
 
             # if object's velocity < 0.02, stop the robot
-            while vel < 0.02 and switch_controller == 2:
-                target_joint = env.joint_position()[:7]
+            while vel < 0.01 and switch_controller == 2:
+                target_joint = inverse_kinematics(ee_curr_pos, target_ee_ori, env.joint_position()[:7])
+                # target_joint = env.joint_position()[:7]
                 switch_controller = 3
                 break
 
             if count > 0: # controller 1, in-contact phase
                 # 1.Joint position-based PD controller
-                dx = (target_joint - env.joint_position()[:7]) * 0.02
-                desired_qpos = env.joint_position()[:7] + dx
-                position_error = desired_qpos - env.joint_position()[:7]
+                position_error = (target_joint - env.joint_position()[:7]) * 0.01
                 vel_pos_error = -env.joint_velocities()[:7]
                 desired_torque = (np.multiply(np.array(position_error), np.array(kp)) + np.multiply(vel_pos_error, kd))
-                
-                # 2.End effector position-based PD controller
-                # curr_joint_pos = inverse_kinematics(curr_ee, target_ee_ori, env.joint_position()[:7])
-                # dx_joint = (target_joint - curr_joint_pos) * 0.02
-                # desired_qpos = curr_joint_pos + dx_joint
-                # position_error = desired_qpos - curr_joint_pos
-                # vel_pos_error = -env.joint_velocities()[:7]
-                # desired_torque = (np.multiply(np.array(position_error), np.array(kp)) + np.multiply(vel_pos_error, kd))
-                
             else: # controller 2, pre-move phase
                 F, error = compute_ts_force(curr_pos, curr_ori, target_pos, original_ori, curr_vel, curr_omg, target_vel)
                 desired_torque = np.dot(env.get_ee_jacobian("ee_joint").T, F).flatten().tolist()
                 target_ee_pos, target_ee_ori = env.get_ee_pose() # record the ee position and pose at the moment of contact
-
 
             # Return desired torques plus gravity compensations
             MassMatrix = joint_controller.dynamics.MassMatrix()
@@ -225,7 +223,7 @@ def impedance_control_integration(ctrl_rate):
                 diff_ori = quatdiff_in_euler(curr_ori, target_ee_ori)
                 print(f"in contact 2 diff ori: {diff_ori}")
                 ee_curr_pos, ee_curr_ori = env.get_ee_pose()
-                if (np.absolute(diff_ori) > 0.03).any():
+                if (np.absolute(diff_ori) > 0.08).any():
                     adjust_joint_pos = inverse_kinematics(ee_curr_pos, target_ee_ori, env.joint_position()[:7])
                     env.set_initial_pos(adjust_joint_pos)
 
@@ -238,16 +236,15 @@ def impedance_control_integration(ctrl_rate):
 
             step += 1
 
+
 def go_to_initial_pos():
     global env, robot, joint_controller
     # initial_pos = np.array([0.0, 0.85, 0.0, -1.7, -1.62, 1.6, 0.0])
     initial_pos = np.array([0.0, 0.85, -0.2, -1.7, -1.62, 1.6, 1.0])
     initial_pos2 = np.array([0.0, 0.85, -0.3, -1.7, -1.62, 1.95, 0.3])
 
-
     env.set_initial_pos(initial_pos)
     env.render()
-
     while True:
         jtor, su_flag = joint_controller.impedance_controller_joint()
         robot.set_joint_torque(jtor)
@@ -258,6 +255,7 @@ def go_to_initial_pos():
             break
         sleep(0.005)
 
+
 if __name__ == "__main__":
     model = load_model_from_path("./description/Franka/dynamic_franka_without_gripper_with_object.xml")
     sim = MjSim(model=model)
@@ -267,42 +265,51 @@ if __name__ == "__main__":
     target1 = np.array([0.0, 0.9, 0.0, -1.7, -1.62, 1.6, 0.0])  # target 1
     target2 = np.array([0.0, 0.9, -0.4, -1.7, -1.62, 1.95, 0.3])  # target 2
     joint_controller.set_target(target2)
-
-    go_to_initial_pos()
-    print("initial position")
+        
+    # inverse kinematics solver
+    ik_target_pos = None
+    ik_target_quat = None
+    ik_current_pos = None
+    ik_joint_pos = None
+    ik_physics = mujoco.Physics.from_xml_string(_ARM_XML)
+    physics2 = ik_physics.copy(share_model=True)
 
     rospy.init_node('talker', anonymous=True)
-
     pub_d = rospy.Publisher('distance', Float32, queue_size=10)  # the distance between object and robot's end effector
     pub_f = rospy.Publisher('contact_force', Float32, queue_size=10)  # the contact force
-
     pub_vel = rospy.Publisher('object_velocity', Float32, queue_size=10)
     pub_robot_vel = rospy.Publisher('robot_velocity', Float32, queue_size=10)
-
+    run_controller = False
     force_norm = 0
-    env.sim.data.xfrc_applied[env.sim.model.body_name2id("object"), :] = np.array([0, 40, 0, 0, 0, 0])
+    go_to_initial_pos() # go to initial position
+    
+    force_norm = 0
+    env.sim.data.xfrc_applied[env.sim.model.body_name2id("object"), :] = np.array([0, 20, 0, 0, 0, 0])
     print("applied force!")
     for i in range(2000):
-        env.sim.step()
-        env.render()
-        # vel = math.sqrt(env.joint_velocities()[7] ** 2 + env.joint_velocities()[8] ** 2)
-        vel = env.joint_velocities()[7] 
-        pub_vel.publish(vel)
-        pub_f.publish(force_norm)
-        # print("applied force!")
         joint_pos = env.get_site_pos("ee_joint")
         object_pos = env.get_site_pos("obj_contact")
         dis = np.sqrt(np.sum(np.square(joint_pos - object_pos)))
+
         if dis <= 1:
             env.sim.data.xfrc_applied[env.sim.model.body_name2id("object"), :] = np.array([0, 0, 0, 0, 0, 0])
+        if dis >= 0:
+            vel_pos_error_initial = -env.joint_velocities()[:7]
+            position_error_initial = target2 - env.joint_position()[:7]
+            desired_torque_initial = (np.multiply(np.array(position_error_initial), np.array(1800)) + np.multiply(vel_pos_error_initial, 400))
+            MassMatrix_intial = joint_controller.dynamics.MassMatrix()
+            desired_torque_initial = MassMatrix_intial.dot(desired_torque_initial) + joint_controller.dynamics.gravityforces(robot.joint_pos)
+            robot.set_joint_torque(desired_torque_initial)
         if dis < 0.3:
+            run_controller = True
             break
-        sleep(0.001)
+        
+        vel = env.joint_velocities()[7] 
+        pub_vel.publish(vel)
+        pub_f.publish(force_norm)
+        env.sim.step()
+        env.render()
 
-    if dis <= 0.3:
-        run_controller = True
-    else:
-        run_controller = False
 
     ctrl_rate = 1 / env.sim.model.opt.timestep
     render_rate = 100
@@ -315,9 +322,14 @@ if __name__ == "__main__":
 
     ctrl_thread = threading.Thread(target=impedance_control_integration, args=[ctrl_rate])  # multi-thread
     ctrl_thread.start()
-
+    
+    # ik_thread = threading.Thread(target=inverse_kinematics_thread)
+    # ik_thread.start()
+    
+    
     now_r = time.time()
     i = 0
+    
 
     while i < len(target_y_vel):
 
